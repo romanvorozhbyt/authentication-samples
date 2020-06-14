1f144522b921b9833fd6ab0ad2f6f215abb20268
Roman Vorozhbyt <romanvorozhbyt199898@gmail.com>
2020-06-14 16:27:06 +0300

    Extract Authentication and swagger configuration into extensions methods

--- AuthTraining/ServiceCollectionExtensions.cs
+++ AuthTraining/ServiceCollectionExtensions.cs
@@ -1,6 +1,15 @@
 ﻿using AuthTraining.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using WeatherForecast.Data;
+using WeatherForecastApi.Models;
 
 namespace AuthTraining
 {
@@ -11,6 +20,56 @@ namespace AuthTraining
             services.AddTransient<IIdentityProvider, IdentityProvider>();
             services.AddTransient<IAuthenticationTokenProvider, JwtTokenProvider>();
             services.AddData();
+            return services;
+        }
+
+        public static IServiceCollection AddBearerAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+           .AddJwtBearer(options =>
+           {
+               options.RequireHttpsMetadata = false;
+               options.SaveToken = true;
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+                   ValidIssuer = configuration[ApplicationSettingsConstants.JwtIssuer],
+                   ValidAudience = configuration[ApplicationSettingsConstants.JwtAudience],
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration[ApplicationSettingsConstants.JwtSecretKey]))
+               };
+               options.Events = new JwtBearerEvents
+               {
+                   OnAuthenticationFailed = context =>
+                   {
+                       if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                       {
+                           context.Response.Headers.Add("Token-Expired", "true");
+                       }
+                       return Task.CompletedTask;
+                   }
+               };
+           });
+           return services;
+        }
+
+        public static IServiceCollection AddSwaggerBearerAuthentication(this IServiceCollection services)
+        {
+            services.AddSwaggerDocument(document =>
+            {
+                document.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Name = "Authorization",
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Description = "Type into the textbox: Bearer {your JWT token}."
+                });
+
+                document.OperationProcessors.Add(
+                    new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+            });
             return services;
         }
     }
--- AuthTraining/Startup.cs
+++ AuthTraining/Startup.cs
@@ -1,23 +1,10 @@
 using AuthTraining.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using NSwag;
-using NSwag.AspNetCore;
-using NSwag.Generation.Processors.Security;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WeatherForecastApi.Extensions;
 using WeatherForecastApi.Middlewares;
-using WeatherForecastApi.Models;
 
 namespace AuthTraining
 {
@@ -34,52 +21,13 @@ namespace AuthTraining
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-            {
-                options.RequireHttpsMetadata = false;
-                options.SaveToken = true;
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = Configuration[ApplicationSettingsConstants.JwtIssuer],
-                    ValidAudience = Configuration[ApplicationSettingsConstants.JwtAudience],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration[ApplicationSettingsConstants.JwtSecretKey]))
-                };
-                options.Events = new JwtBearerEvents
-                {
-                    OnAuthenticationFailed = context =>
-                    {
-                        if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
-                        {
-                            context.Response.Headers.Add("Token-Expired", "true");
-                        }
-                        return Task.CompletedTask;
-                    }
-                };
-
-            });
+            services.AddBearerAuthentication(Configuration);
             services.AddAuthorization(config =>
             {
                 config.AddPolicy(AuthorizationPolicies.Admin, AuthorizationPolicies.AdminPolicy());
                 config.AddPolicy(AuthorizationPolicies.User, AuthorizationPolicies.UserPolicy());
             });
-            services.AddSwaggerDocument(document =>
-            {
-                document.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
-                {
-                    Type = OpenApiSecuritySchemeType.ApiKey,
-                    Name = "Authorization",
-                    In = OpenApiSecurityApiKeyLocation.Header,
-                    Description = "Type into the textbox: Bearer {your JWT token}."
-                });
-
-                document.OperationProcessors.Add(
-                    new AspNetCoreOperationSecurityScopeProcessor("JWT"));
-            });
+            services.AddSwaggerBearerAuthentication();
             services.AddAuthTraining();
         }
 
@@ -109,6 +57,5 @@ namespace AuthTraining
                 endpoints.MapControllers();
             });
         }
-
     }
 }
